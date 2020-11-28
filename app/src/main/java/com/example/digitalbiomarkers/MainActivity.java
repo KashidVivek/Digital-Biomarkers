eeae85c3e9ee96f23921bc58a08d97d2d97a0387
KashidVivek <vivek.kashid@viit.ac.in>
2020-11-27 19:45:54 -0500

    tone analysis of db

--- app/src/main/java/com/example/digitalbiomarkers/MainActivity.java
+++ app/src/main/java/com/example/digitalbiomarkers/MainActivity.java
@@ -32,10 +32,13 @@ import com.ibm.watson.tone_analyzer.v3.model.ToneOptions;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     private long screenTime = 0;
     private int numUnlocks = 0;
+    public  String tone;
+    public String textToAnalyse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,30 +48,44 @@ public class MainActivity extends AppCompatActivity {
         IamAuthenticator authenticator = new IamAuthenticator("Ufr3FL9wHAotv_f7QItN7Q_EKvUHkpK-B88J5USZWloR");
         ToneAnalyzer toneAnalyzer = new ToneAnalyzer("8.6.3",authenticator);
         toneAnalyzer.setServiceUrl("https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/94baf436-8819-49d9-af67-cf6c3fc3345c");
-
+        String selection = Keyboard_Provider.Keyboard_Data._ID + " IN (SELECT "
+                + Keyboard_Provider.Keyboard_Data._ID +"- 1"+ " FROM "
+                + "keyboard" + " WHERE "
+                + Keyboard_Provider.Keyboard_Data.BEFORE_TEXT + "=?)";
+        String sel =  Keyboard_Provider.Keyboard_Data.BEFORE_TEXT + "=?";
+        String[] selectionargs = new String[]{""};
         Button analyzeButton = (Button)findViewById(R.id.button);
-
+        TextView sTime = (TextView) findViewById(R.id.sampleText);
         analyzeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RetrieveFeedTask emotion = new RetrieveFeedTask(getApplicationContext());
-                emotion.execute();
+                Cursor keyboardCursor = getContentResolver().query(Keyboard_Provider.Keyboard_Data.CONTENT_URI,null, selection,selectionargs,null);
+                List<String> text = new ArrayList<String>();
+
+                if (keyboardCursor!=null){
+                    keyboardCursor.moveToFirst();
+                    do{
+                        text.add(keyboardCursor.getString(keyboardCursor.getColumnIndexOrThrow(Keyboard_Provider.Keyboard_Data.CURRENT_TEXT)));
+                    }while(keyboardCursor.moveToNext());
+                    textToAnalyse = text.toString().replace("[","").replace("]","");
+//                    Toast.makeText(getApplicationContext(), "MainActivity"+textToAnalyse, Toast.LENGTH_SHORT).show();
+                    RetrieveFeedTask emotion = new RetrieveFeedTask(getApplicationContext(),textToAnalyse);
+                    try {
+                        tone = emotion.execute().get();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    sTime.setText(tone);
+                    getApplicationContext().getContentResolver().delete(Keyboard_Provider.Keyboard_Data.CONTENT_URI,null,null);
+                }
             }
         });
-
-        Button connectDb = (Button)findViewById(R.id.connectDB);
-        connectDb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ConnectDB cdb= new ConnectDB(getApplicationContext());
-                cdb.execute();
-
-            }
-        });
-
         Aware.startAWARE(this);
-//        Aware.joinStudy(this,"http://167.71.59.111:8080/index.php/1/4lph4num3ric");
-        Aware.joinStudy(this,"http://10.20.188.9:8080/index.php/1/digitalHealthF2020");
+//        Aware.joinStudy(getApplicationContext(),"http://167.71.59.111:8080/index.php/1/4lph4num3ric");
+        sTime.setText(Boolean.toString(Aware.isStudy(getApplicationContext())));
+//        Aware.joinStudy(this,"http://10.20.188.9:8080/index.php/1/digitalHealthF2020");
         Aware.setSetting(this, Aware_Preferences.STATUS_SCREEN,true);
         Aware.setSetting(this, Aware_Preferences.STATUS_KEYBOARD,true);
         Aware.setSetting(this,"status_plugin_device_usage",true);
@@ -80,33 +97,36 @@ public class MainActivity extends AppCompatActivity {
 
         Applications.isAccessibilityServiceActive(getApplicationContext());
 
-        String selection = Keyboard_Provider.Keyboard_Data._ID + " IN (SELECT "
-                            + Keyboard_Provider.Keyboard_Data._ID +"- 1"+ " FROM "
-                            + "keyboard" + " WHERE "
-                            + Keyboard_Provider.Keyboard_Data.BEFORE_TEXT + "=?)";
-        String sel =  Keyboard_Provider.Keyboard_Data.BEFORE_TEXT + "=?";
-        String[] selectionargs = new String[]{""};
-
         Cursor cursor = getContentResolver().query(Screen_Provider.Screen_Data.CONTENT_URI,null, null,null,null);
         Cursor deviceUsageCursor = getContentResolver().query(Provider.DeviceUsage_Data.CONTENT_URI,null, null,null,null);
         Cursor CommsCursor = getContentResolver().query(Communication_Provider.Calls_Data.CONTENT_URI,null,null,null,null);
         Cursor AppsCursor = getContentResolver().query(Applications_Provider.Applications_Foreground.CONTENT_URI,null,null,null,null);
-        Cursor keyboardCursor = getContentResolver().query(Keyboard_Provider.Keyboard_Data.CONTENT_URI,null, selection,selectionargs,null);
-        List<String> text = new ArrayList<String>();
-        TextView sTime = (TextView) findViewById(R.id.sampleText);
-
-        if (keyboardCursor!=null){
-            keyboardCursor.moveToFirst();
-            do{
-                text.add(keyboardCursor.getString(keyboardCursor.getColumnIndexOrThrow(Keyboard_Provider.Keyboard_Data.CURRENT_TEXT)));
-            }while(keyboardCursor.moveToNext());
-            sTime.setText(text.toString().replace("[","").replace("]",""));
-        }
-        else{
-            sTime.setText("NOT WORKING");
-        }
+//        Cursor keyboardCursor = getContentResolver().query(Keyboard_Provider.Keyboard_Data.CONTENT_URI,null, selection,selectionargs,null);
+//        List<String> text = new ArrayList<String>();
+//
+//
+//        if (keyboardCursor!=null){
+//            keyboardCursor.moveToFirst();
+//            do{
+//                text.add(keyboardCursor.getString(keyboardCursor.getColumnIndexOrThrow(Keyboard_Provider.Keyboard_Data.CURRENT_TEXT)));
+//            }while(keyboardCursor.moveToNext());
+//            setAnalysisText(text.toString().replace("[","").replace("]",""));
+////            sTime.setText(text.toString().replace("[","").replace("]",""));
+////            sTime.setText(tone);
+//        }
+//        else{
+//            sTime.setText("NOT WORKING");
+//        }
 
     }
+
+    private void setAnalysisText(String replace) {
+        textToAnalyse = replace;
+    }
+
+//    public static void setTone(String setterTone){
+//        tone = setterTone;
+//    }
 
 }
 
--- app/src/main/java/com/example/digitalbiomarkers/RetrieveFeedTask.java
+++ app/src/main/java/com/example/digitalbiomarkers/RetrieveFeedTask.java
@@ -15,27 +15,19 @@ import com.ibm.watson.tone_analyzer.v3.model.ToneOptions;
 
  public class RetrieveFeedTask extends AsyncTask<Void,Void,String> {
     private Context context;
+    public String textToAnalyse;
 
-    public RetrieveFeedTask(Context context){
+    public RetrieveFeedTask(Context context,String textToAnalyse){
         this.context = context;
+        this.textToAnalyse = textToAnalyse;
     }
     @Override
     protected String doInBackground(Void... voids) {
         IamAuthenticator authenticator = new IamAuthenticator("Ufr3FL9wHAotv_f7QItN7Q_EKvUHkpK-B88J5USZWloR");
         ToneAnalyzer toneAnalyzer = new ToneAnalyzer("2017-09-21",authenticator);
         toneAnalyzer.setServiceUrl("https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/94baf436-8819-49d9-af67-cf6c3fc3345c");
-        String textToAnalyze = "I know the times are difficult! Our sales have been "
-                + "disappointing for the past three quarters for our data analytics "
-                + "product suite. We have a competitive data analytics product "
-                + "suite in the industry. But we need to do our job selling it! "
-                + "We need to acknowledge and fix our sales challenges. "
-                + "We can’t blame the economy for our lack of execution! "
-                + "We are missing critical sales opportunities. "
-                + "Our product is in no way inferior to the competitor products. "
-                + "Our clients are hungry for analytical tools to improve their "
-                + "business outcomes. Economy has nothing to do with it.";
         ToneOptions toneOptions = new ToneOptions.Builder()
-                .text(textToAnalyze)
+                .text(textToAnalyse)
                 .build();
 
         ToneAnalysis toneAnalysis = toneAnalyzer.tone(toneOptions).execute().getResult();
@@ -48,5 +40,6 @@ import com.ibm.watson.tone_analyzer.v3.model.ToneOptions;
     protected void onPostExecute(String msg) {
         super.onPostExecute(msg);
         Toast.makeText(context, "Emotion:" + msg, Toast.LENGTH_SHORT).show();
+//        MainActivity.setTone(msg);
     }
 }
